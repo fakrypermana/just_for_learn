7462440ba97a1e023431c9233ed1a2fbed0a0a63
putri <borongajayuk@gmail.com>
2018-05-24 16:32:39 +0700

    edit webview content

--- app/src/main/java/id/teknologi/teknologiid/feature/thread/ThreadDetailActivity.java
+++ app/src/main/java/id/teknologi/teknologiid/feature/thread/ThreadDetailActivity.java
@@ -40,7 +40,8 @@ public class ThreadDetailActivity extends BaseActivity {
         String url = "file:///android_asset/index.html";
         WebView web =(WebView)findViewById(R.id.wv_detileThread);
         web.setWebViewClient(new WebViewClient());
-        web.loadUrl("http://dev.teknologi.id/forum/question/5/bagaimana-cara-memusatkan-%3Cdiv%3E-secara-horizontal-di-%3Cdiv%3E-lain%3F");
+        String data = "<p style=\"text-align: center;\"><iframe width=\" 480\" height=\"270\" src=\"https://www.youtube.com/embed/jF9VULLzwVk?feature=oembed\" frameborder=\"0\" allowfullscreen=\"allowfullscreen\"></iframe><img src=\"https://ph-files.imgix.net/d4e2ded5-b3d6-46ce-9d34-7464dae76567?auto=format&amp;auto=compress&amp;codec=mozjpeg&amp;cs=strip\" alt=\"\"><img src=\"https://ph-files.imgix.net/727db047-c452-4b2b-a44f-4eba4267a3e7?auto=format&amp;auto=compress&amp;codec=mozjpeg&amp;cs=strip\" alt=\"\"><img src=\"https://ph-files.imgix.net/f10a4ae9-6d62-46cd-a5c0-09547ba8ba7a?auto=format&amp;auto=compress&amp;codec=mozjpeg&amp;cs=strip\" alt=\"\"><img src=\"https://ph-files.imgix.net/79b4a3b6-3857-4353-85cd-7da5e74d4bbf?auto=format&amp;auto=compress&amp;codec=mozjpeg&amp;cs=strip\" alt=\"\"><img src=\"https://ph-files.imgix.net/28969b61-6983-4a5c-9717-39654aef9834?auto=format&amp;auto=compress&amp;codec=mozjpeg&amp;cs=strip\" alt=\"\"></p><p style=\"text-align: center;\">Prisma is a performant open-source GraphQL ORM-like layer doing the heavy lifting in your GraphQL server. It turns your database into a GraphQL API which can be consumed by your resolvers via GraphQL bindings.</p>";
+        web.loadData(data,"text/html", "UTF-8");
     }
 
     public static void start(Activity activity, int id, String slug){
